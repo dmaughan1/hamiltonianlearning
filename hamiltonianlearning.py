902e031ef7114241266f4dcb35d5460835a80e3f
dmaughan1 <34294361+dmaughan1@users.noreply.github.com>
2020-04-10 23:12:09 -0700

    Update hamiltonianlearning.py
    Replaced the formHamiltonian code so now its 2x faster
    Added comments and cleaned up code

--- hamiltonianlearning.py
+++ hamiltonianlearning.py
@@ -1,46 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 20:18:03 2020
+
+@author: david
+"""
+
 import numpy as np
 from scipy.linalg import expm
 import time
 
 from hlFunctions import *
 
-# In case you want to save results
-#timestr = time.strftime("%Y%m%d-%H%M")
-#myFile = open(timestr + "hamiltonianLearningTest.txt", "w")
-np.random.seed(8)
+# I used a seed to have reproducible results for checking speed
+#np.random.seed(8)
 
-# Define variables
-numOfQuantumStates = 4 # Should be equal to 4 if you want to simulate hyperfine or cyclobutadiene
-symmetricBasis = generateSymmetricBasis(numOfQuantumStates)
+# Choose which hamiltonian you want to learn through the variable 'hamnum'
+# 1 - hyperfine splitting of hydrogen
+# 2 - cyclobutadiene
+# 3 - random hamiltonian
+hamnum = 3
+if hamnum == 1:
+    numOfQuantumStates = 4
+    trueHamiltonianWeights = np.array([1.0,0.0,-1.0,0.0,2.0,-1.0,0.0,0.0,0.0,1.0])
+elif hamnum == 2:
+    numOfQuantumStates = 4
+    b = 1.0 
+    trueHamiltonianWeights = np.array([0,b,0.0,0.0,b,0.0,b,0.0,b,0.0]) 
+else:
+    numOfQuantumStates = 3 # the hamiltonian will be this size squared; must be 3 or larger
+    trueHamiltonianWeights = np.random.uniform(-1.0,1.0,int(numOfQuantumStates*(numOfQuantumStates+1)/2))
 
-# Choose which hamiltonian you want to attempt to learn. We have included
-# the hamiltonians for hyperfine splitting, the cyclobutadiene molecule
-# and a totally random hamiltonian
+# NOTE: The true hamiltonian is treated as a black box in the code
+# It is only used to generate the output data, which would normally be measured in a lab
 
-# Define the true hamiltonian, which is treated as a black box in the code
-# It is only used to generate the output data
 
-""" PREDEFINED HAMILTONIANS
-# Hamiltonian for hyperfine ### numOfQuantumStates must equal 4
-#trueHamiltonianWeights = np.array([1.0,0.0,-1.0,0.0,2.0,-1.0,0.0,0.0,0.0,1.0]) 
-
-# Hamiltonian for cyclobutadiene molecule ### numOfQuantumStates must equal 4
-#b = 1.0 # This value and hamiltonian comes from stack exchange post Dave put in our Slack
-#trueHamiltonianWeights = np.array([0,b,0.0,0.0,b,0.0,b,0.0,b,0.0]) 
-"""
-# Random Hamiltonian
-trueHamiltonianWeights = np.random.uniform(-1.0,1.0,int(numOfQuantumStates*(numOfQuantumStates+1)/2))
-
-# Randomly initialize the learning Hamiltonian. We use optimization techniques to try get the learningHamiltonian as
-# close as possible to the true Hamiltonian
+# Randomly initialize the learning Hamiltonian. We use optimization techniques to get the
+# learningHamiltonian as close as possible to the true Hamiltonian
 learningHamiltonianWeights = np.random.uniform(-1.0,1.0,int(numOfQuantumStates*(numOfQuantumStates+1)/2))
-##learningHamiltonianWeights = np.random.uniform(-1.0,1.0,int(numOfQuantumStates*(numOfQuantumStates+1)/2))
 
-#myFile.write("True Weights: " + str(trueHamiltonianWeights) +"\n")
-#myFile.write("Learning Weights: " + str(learningHamiltonianWeights) +"\n")
 
-trueHamiltonian = formHamiltonian(trueHamiltonianWeights,symmetricBasis,numOfQuantumStates)
-initLearningHamiltonian = formHamiltonian(learningHamiltonianWeights,symmetricBasis,numOfQuantumStates)
+# for printing
+trueHamiltonian = formHamiltonian(trueHamiltonianWeights,numOfQuantumStates)
+initLearningHamiltonian = formHamiltonian(learningHamiltonianWeights,numOfQuantumStates)
 
 print("Initial learning Hamiltonian")
 print(initLearningHamiltonian)
@@ -52,51 +53,46 @@ print("")
 # Define the times you want to train at
 # The complexity grows linearly with the length of the timeList
 timeList = [3.141592654/4.0,3.141592654/8.0,3.141592654/16.0] #[np.random.uniform(0.0,2.0),np.random.uniform(0.0,2.0)]#
-#myFile.write("Time List: " + str(timeList) + "\n")
+
 # Define an initial loss. It is quickly overwritten, but we need an inital value to enter the while loop
 loss = 1.0
 
 # The maximium number of iterations before you quit 
-MAXITERS = 4000
+MAXITERS = 1000
 
 # Current number of iterations of gradient descent
 iters = 0
 
 learningRate = .01
 
-# Some initial data info
-#print("Initial Weights: ", learningHamiltonianWeights)
-#print("Desired Weights: ", trueHamiltonianWeights)
-
 # velocities is used in momentum based gradient descent. This makes the code faster
 velocities = np.zeros(trueHamiltonianWeights.shape)
 
-threshold = 2e-6 #This convergence threshold is a little finicky. Depends on the problem you are solving
 dampening = 0.8 #This is a momentum parameter. Setting it to zero, you recover vanila gradient descent. .8 or.9 is reasonable
 
 # Define input and output data
 # inputData is a numpy array
-# outputData is a list of numpy arrays. The list length is equal to the length on timeList
+# outputData is a list of numpy arrays. The list length is equal to the length of timeList
 inputData = createNumChooseTwoInputData(numOfQuantumStates)
 outputData = []
 for index in range(0,len(timeList)):
     outputData.append( calculateCircuit(trueHamiltonian,timeList[index]) @ inputData)
 
-# Set the initial gradient to something large enough to enter the while loop
-gradient = np.ones(trueHamiltonianWeights.shape)
+lossList = [] # store loss at every epoch
 
 # perform gradient descent
-lossList = []
-while loss > 1.5e-11 * len(timeList) * numOfQuantumStates**2.0: #((np.linalg.norm(gradient) > threshold) or (np.linalg.norm(velocities) > threshold))  and
-    gradient = calculateGradient(calculateCost, learningHamiltonianWeights, trueHamiltonianWeights, symmetricBasis, numOfQuantumStates, timeList, inputData, outputData)
+comptime = time.time() # to compare speeds
+while loss > 1.5e-7 * len(timeList) :#* numOfQuantumStates**2.0: #((np.linalg.norm(gradient) > threshold) or (np.linalg.norm(velocities) > threshold))  and
+    gradient = calculateGradient(calculateCost, learningHamiltonianWeights, trueHamiltonianWeights, numOfQuantumStates, timeList, inputData, outputData)
     velocities = dampening*velocities + learningRate*gradient
-    loss = calculateCost(learningHamiltonianWeights, trueHamiltonianWeights, symmetricBasis, numOfQuantumStates, timeList, inputData, outputData)
+    loss = calculateCost(learningHamiltonianWeights, trueHamiltonianWeights, numOfQuantumStates, timeList, inputData, outputData)
 
     lossList.append(loss)
     learningHamiltonianWeights -= velocities
     iters += 1
     if iters % 10 == 0:
-        learningHamiltonian = formHamiltonian(learningHamiltonianWeights,symmetricBasis,numOfQuantumStates)
+        # check how far from the truth you are
+        learningHamiltonian = formHamiltonian(learningHamiltonianWeights,numOfQuantumStates)
         deltaEnergy = learningHamiltonian[0][0] - trueHamiltonian[0][0]
         for i in range(numOfQuantumStates):
             learningHamiltonian[i][i] -= deltaEnergy
@@ -105,39 +101,26 @@ while loss > 1.5e-11 * len(timeList) * numOfQuantumStates**2.0: #((np.linalg.nor
         maxnorm = np.amax(diff)
 
         print("Iters: ", iters, " LOSS: ", loss, "Max Norm: ", maxnorm)
-        #print("Weights")
-        #print(learningHamiltonianWeights)
-        #print("")
+
     elif iters > MAXITERS:
-        print("normVel:",np.linalg.norm(velocities), "normGrad:", np.linalg.norm(gradient), "COST:",loss)
-        #print(learningHamiltonianWeights)
+        print('did not converge')
         break
-    #""" commented out for SPSA
+
     # If you can lower the cost by multiplying the hamiltonian by -1, do it
     if iters % 501 == 0:
-        if calculateCost(-learningHamiltonianWeights, trueHamiltonianWeights, symmetricBasis, numOfQuantumStates, timeList, inputData, outputData) < loss:
+        if calculateCost(-learningHamiltonianWeights, trueHamiltonianWeights, numOfQuantumStates, timeList, inputData, outputData) < loss:
             learningHamiltonianWeights = -learningHamiltonianWeights
-    if np.linalg.norm(velocities) < threshold and np.linalg.norm(gradient) < threshold:
-        print("Did not converge!!!!!!!")
-        break
-   # """
 
-    
 
 
 # print final stats
-finalLearningHamiltonian = formHamiltonian(learningHamiltonianWeights, symmetricBasis, numOfQuantumStates)
-
-print("Final iters: ", iters)
-print("Final cost: ", loss)
+finalLearningHamiltonian = formHamiltonian(learningHamiltonianWeights, numOfQuantumStates)
 print("Final learning Hamiltonian")
 print(finalLearningHamiltonian)
+print("training took: ",time.time()-comptime, " seconds")
+print("Final iters: ", iters)
+print("Final cost: ", loss)
 
-#myFile.write("Final iters: " + str(iters) + "\n")
-#myFile.write("Final cost: " + str(loss) + "\n")
-#myFile.write("final learning weights: " + str(learningHamiltonianWeights)) 
-
-#myFile.close()
 
 # plot the iterations vs loss
 import matplotlib.pyplot as plt
@@ -145,10 +128,10 @@ import matplotlib.pyplot as plt
 plt.plot(lossList)
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
+plt.title('Loss vs Epochs')
 plt.show()
 
 #### MAX NORM
-
 # To quantify the difference between the hamitonians we have to handle
 # the ambigquity of a scalar shift on the diagonal terms.
 deltaEnergy = finalLearningHamiltonian[0][0] - trueHamiltonian[0][0]
