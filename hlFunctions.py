69fd822291655dc771f27c17ffed772fbd7603ce
dmaughan1 <34294361+dmaughan1@users.noreply.github.com>
2020-04-10 23:13:23 -0700

    Update hlFunctions.py
    Made formHamiltonian code faster
    Added comments and cleaned up the code

--- hlFunctions.py
+++ hlFunctions.py
@@ -1,30 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 20:14:02 2020
+
+@author: david
+"""
+
 import numpy as np
 from scipy.linalg import expm
 
 """
-The idea of this code is to try to learn an arbitrary (random) Hamiltonian. It is hard to try to find many specific models to see if our algorithm learns them. We can try to learn an arbitrary Hamiltonian. This way we can easily see when our algorithm succeeds and fails.  
-
 We will have a trueHamiltonian and a learningHamiltonian. We will adjust the entries of the learningHamiltonian using gradient descent to arrive at the trueHamiltonian
-
 """
 
-def generateSymmetricBasis(n):
-    #generate a basis for the vector space of symmetric matrices
-    #returns them as a list
-    basis = []
-    for i in range(n):
-        for j in range(i+1):
-            tempMatrix = np.zeros((n,n))
-            tempMatrix[i][j] = 1
-            tempMatrix[j][i] = 1
-            basis.append(tempMatrix)
-    return basis
 
-def formHamiltonian(myNewWeights, myBasis, numOfQuantumStates):
-    # Use the basis and weights and return a hamiltonian
+def formHamiltonian(myNewWeights, numOfQuantumStates):
+    # turn the 1 dim of weights into a 2d hamiltonian matrix
     newHamiltonian = np.zeros((numOfQuantumStates, numOfQuantumStates))
-    for i in range(len(myNewWeights)):
-        newHamiltonian += myNewWeights[i]*myBasis[i]
+    count = 0
+    for i in range(0,numOfQuantumStates):
+        for j in range(0,i+1):
+            newHamiltonian[i,j] = myNewWeights[count]
+            newHamiltonian[j,i] = myNewWeights[count]
+            count += 1
+    #newHamiltonian = newHamiltonian + newHamiltonian.T - 
     return newHamiltonian
 
 def calculateCircuit(myHamiltonian,myTime):
@@ -58,14 +56,14 @@ def createNumChooseTwoInputData(numOfQuantumStates):
             rowNum += 1
     return numChooseTwoInputData/np.sqrt(2)
 
-def calculateCost(weights, trueWeights, myBasis, numOfQuantumStates, timeList, inputData, outputData):
+def calculateCost(weights, trueWeights, numOfQuantumStates, timeList, inputData, outputData):
     cost = 0.0
     if len(timeList) != len(outputData):
         print("Error! list lengths do not match")
     for index in range(len(outputData)):
-        learningHamiltonian = formHamiltonian(weights, myBasis, numOfQuantumStates)
+        learningHamiltonian = formHamiltonian(weights, numOfQuantumStates)
         learningCircuit = calculateCircuit(learningHamiltonian,timeList[index])
-        trueHamiltonian = formHamiltonian(trueWeights, myBasis, numOfQuantumStates)
+        trueHamiltonian = formHamiltonian(trueWeights, numOfQuantumStates)
         trueCircuit = calculateCircuit(trueHamiltonian, timeList[index])
         trueCircuitConjT = np.conj(trueCircuit).T
         
@@ -87,34 +85,37 @@ def calculateCost(weights, trueWeights, myBasis, numOfQuantumStates, timeList, i
             cost += 1 - abs(costMatrix2[i_][i_])**2.0
 
 
-        #Train on uniform superposition
+        # Train on uniform superposition
+        # This could be added to the input data, it was convient to have here
         uniformSuperposition = (np.ones(numOfQuantumStates).T)/np.sqrt(numOfQuantumStates)
 
         predictedUniformSuperposition = learningCircuit @ uniformSuperposition
         trueUniformSuperposition = trueCircuit @ uniformSuperposition
 
+        # I didn't bother to normalize the cost for numerical underflow reasons
         cost += 1 - abs(np.dot(np.conj(trueUniformSuperposition).T,predictedUniformSuperposition))**2.0
 
     return cost
     
 
-def calculateGradient(myCostFunction, learningHamiltonianWeights, trueHamiltonianWeights, symmetricBasis, numOfQuantumStates, timeList, inputData, outputData):
+def calculateGradient(myCostFunction, learningHamiltonianWeights, trueHamiltonianWeights, numOfQuantumStates, timeList, inputData, outputData):
     # TODO: for a final optimization, precalculate the denom instead of doing the division every time
     gradient = np.zeros(len(learningHamiltonianWeights))
     alpha = 1e-10 #This is our numerical derivative stepsize
     for i in range(len(learningHamiltonianWeights)):
         myArray = np.zeros(len(learningHamiltonianWeights))
         myArray[i] = 1.0
-        gradient[i] = ( myCostFunction(learningHamiltonianWeights + alpha*myArray, trueHamiltonianWeights, symmetricBasis, numOfQuantumStates, timeList, inputData, outputData) - myCostFunction(learningHamiltonianWeights - alpha*myArray, trueHamiltonianWeights, symmetricBasis, numOfQuantumStates, timeList, inputData, outputData) )/(2.0*alpha)
+        gradient[i] = ( myCostFunction(learningHamiltonianWeights + alpha*myArray, trueHamiltonianWeights, numOfQuantumStates, timeList, inputData, outputData) - myCostFunction(learningHamiltonianWeights - alpha*myArray, trueHamiltonianWeights, numOfQuantumStates, timeList, inputData, outputData) )/(2.0*alpha)
     return gradient
 
-def spsa(myCostFunction, learningHamiltonianWeights, trueHamiltonianWeights, symmetricBasis, numOfQuantumStates, timeList, inputData, outputData, iters):
+
+def spsa(myCostFunction, learningHamiltonianWeights, trueHamiltonianWeights, numOfQuantumStates, timeList, inputData, outputData, iters):
+    # It wasn't easy to get great results with spsa, but it would be faster
     pertVec = np.random.uniform(0.1, 1.0, learningHamiltonianWeights.shape)
     gradient = np.zeros(len(learningHamiltonianWeights))
     an = .1
-    numerator = ( myCostFunction(learningHamiltonianWeights + an*pertVec/np.sqrt(iters + 1), trueHamiltonianWeights, symmetricBasis, numOfQuantumStates, timeList, inputData, outputData) - myCostFunction(learningHamiltonianWeights - an*pertVec/np.sqrt(iters + 1), trueHamiltonianWeights, symmetricBasis, numOfQuantumStates, timeList, inputData, outputData) )
+    numerator = ( myCostFunction(learningHamiltonianWeights + an*pertVec/np.sqrt(iters + 1), trueHamiltonianWeights, numOfQuantumStates, timeList, inputData, outputData) - myCostFunction(learningHamiltonianWeights - an*pertVec/np.sqrt(iters + 1), trueHamiltonianWeights, numOfQuantumStates, timeList, inputData, outputData) )
     for i in range(len(pertVec)):
         gradient[i] = numerator/(pertVec[i]*2.0*an)
     return gradient     
-
 
